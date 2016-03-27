50f5da4fcc120917a53c17c7c7b6593e232b84de
makotot <makoto.ttn@gmail.com>
2016-03-27 22:33:18 +0900

    + sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,33 @@
 import gulp from 'gulp'
 import del from 'del'
 import gutil from 'gulp-util'
+import sass from 'gulp-sass'
+import run from 'run-sequence'
 import webpack from 'webpack'
 import WebpackDevServer from 'webpack-dev-server'
 import webpackConfig from './webpack.config.babel'
 
 gulp.task('default', ['webpack-dev-server'])
+gulp.task('serve', () => {
+  run('clean', ['sass', 'webpack-dev-server'], 'watch')
+})
+
+gulp.task('clean', (cb) => {
+  return del([
+    './dist'
+  ], cb)
+})
+
+gulp.task('sass', () => {
+  return gulp
+    .src('./src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch('./src/scss/*.scss', ['sass'])
+})
 
 gulp.task('webpack-dev-server', () => {
   let config = Object.create(webpackConfig)
